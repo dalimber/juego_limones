4e521207ff31db8ffb3b8e0e95e286519062d2ed
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-05 17:30:03 -0500

    Agregar funcion dibujarLimon, bajarLimon, establecer parametrosde dibujo y posicion

--- limones.js
+++ limones.js
@@ -1,28 +1,43 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-const alturaSuelo=25;
-const alturaPersonaje=50;
-const anchoPersonaje=30;
+const ALTURA_SUELO=25;
+const ALTURA_PERSONAJE=50;
+const ANCHO_PERSONAJE=30;
+const ALTO_LIMON=20;
+const ANCHO_LIMON=20;
 
 let personajeX=canvas.width/2;
+let limonX=0;
+let limonY=0;
 function iniciar() 
 {
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
     
 }
 
 function dibujarSuelo () 
 {
     ctx.fillStyle="blue";
-    ctx.fillRect(0,canvas.height-alturaSuelo,canvas.width,alturaSuelo);
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
 }
 function dibujarPersonaje () 
 {
     ctx.fillStyle="yellow";
-    ctx.fillRect(personajeX,canvas.height-(alturaSuelo-5+alturaPersonaje),anchoPersonaje,alturaPersonaje);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO-5+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+function dibujarLimon() 
+{
+    ctx.fillStyle="green";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+}
 
+function bajarLimon() 
+{
+ limonY=limonY+10;
+ actualizarPantalla();
 }
 function moverIzquierda() 
 {
@@ -44,4 +59,5 @@ function actualizarPantalla()
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
