68bae9fac41c5e447cca2efd8c16b4b9e8877959
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-05 02:02:34 -0500

    Crear funciones para dibujar suelo, personaje e inciarlos

--- limones.js
+++ limones.js
@@ -1,2 +1,26 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+const alturaSuelo=25;
+const alturaPersonaje=50;
+const anchoPersonaje=30;
+
+
+function iniciar() 
+{
+    dibujarSuelo();
+    dibujarPersonaje();
+    
+}
+
+function dibujarSuelo () 
+{
+    ctx.fillStyle="blue";
+    ctx.fillRect(0,canvas.height-alturaSuelo,canvas.width,alturaSuelo);
+}
+function dibujarPersonaje () 
+{
+    ctx.fillStyle="yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(alturaSuelo-5+alturaPersonaje),anchoPersonaje,alturaPersonaje);
+
+}
